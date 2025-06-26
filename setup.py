1687e84d85d24b96ec64df856f6f46f0ab27d723
Hajime Nakagami <nakagami@gmail.com>
2025-06-26 12:39:40 +0900

    NO_CYTHON environment

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 import sys
+import os
 
 from setuptools import setup, Extension
 
-try:
-    from Cython.Build import cythonize
-    ext_modules = cythonize([
-            Extension("cymysql.packet", ["cymysql/packet.pyx"]),
-            Extension("cymysql.result", ["cymysql/result.pyx"]),
-            Extension("cymysql.socketwrapper", ["cymysql/socketwrapper.pyx"]),
-            Extension("cymysql.charset", ["cymysql/charset.py"]),
-            Extension("cymysql.converters", ["cymysql/converters.py"]),
-            Extension("cymysql.connections", ["cymysql/connections.py"]),
-            Extension("cymysql.cursors", ["cymysql/cursors.py"]),
-            Extension("cymysql.err", ["cymysql/err.py"]),
-            Extension("cymysql.times", ["cymysql/times.py"]),
-        ],
-        compiler_directives={'language_level': str(sys.version_info[0])},
-    )
-except ImportError:
+if os.environ.get('NO_CYTHON'):
     ext_modules = None
+else:
+    try:
+        from Cython.Build import cythonize
+        ext_modules = cythonize([
+                Extension("cymysql.packet", ["cymysql/packet.pyx"]),
+                Extension("cymysql.result", ["cymysql/result.pyx"]),
+                Extension("cymysql.socketwrapper", ["cymysql/socketwrapper.pyx"]),
+                Extension("cymysql.charset", ["cymysql/charset.py"]),
+                Extension("cymysql.converters", ["cymysql/converters.py"]),
+                Extension("cymysql.connections", ["cymysql/connections.py"]),
+                Extension("cymysql.cursors", ["cymysql/cursors.py"]),
+                Extension("cymysql.err", ["cymysql/err.py"]),
+                Extension("cymysql.times", ["cymysql/times.py"]),
+            ],
+            compiler_directives={'language_level': str(sys.version_info[0])},
+        )
+    except ImportError:
+        ext_modules = None
 
 setup(
     ext_modules=ext_modules,
